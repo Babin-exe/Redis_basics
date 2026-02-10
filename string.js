3d7afa7a166a851e333a3cf1aaf97a64c5016686
Babin <155212596+Babin-exe@users.noreply.github.com>
2026-02-10 11:39:27 +0545

    Update string.js

--- string.js
+++ string.js
@@ -1,8 +1,18 @@
 import client from "./client.js";
 
 async function init() {
-//   await client.set("message:2", "First message");
+  
+    //Set the value 
+  
+   //await client.set("message:2", "First message");
+  
+   //Put Ttl (Time-To-Live. ) 
+  
+  //await client.expire("message:2", 5);
+
+  //Get the value through its key 
   const result = await client.get("message:2");
+  
   console.log("Result->", result);
 }
 init()
