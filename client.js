b64b57f53e30c0363746a23d7759a6cc2b181fa0
Babin <155212596+Babin-exe@users.noreply.github.com>
2026-02-11 22:39:30 +0545

    Update client.js

--- client.js
+++ client.js
@@ -1,5 +1,13 @@
-import { Redis } from "ioredis";
+import { createClient } from "redis";
 
-const client = new Redis();
+const client = createClient({
+  url: "redis://localhost:6379",
+});
+
+client.on("error", (err) => {
+  console.error("Redis Client Error", err);
+});
+
+await client.connect();
 
 export default client;
